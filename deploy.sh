da22884a30d2fa36e1f5e85ba59a1732a237b8e3
Sotatek-CuongTran <cuong.tran@sotatek.com>
2021-11-17 14:58:02 +0700

    feat: zapmini v2

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 yarn hardhat compile
 yarn hardhat deploy --network polygon
 yarn hardhat --network polygon etherscan-verify --api-key TNJ7SSWY1NRNAEAK4XKJ1UHPSATX2ENNW3
+yarn hardhat --network polygon set-router-factory
 yarn hardhat add-intermediate-token --token0 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063 --token1 0xac51C4c48Dc3116487eD4BC16542e27B5694Da1b --intermediate 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619 --network polygon
 yarn hardhat add-intermediate-token --token0 0xc2132d05d31c914a87c6611c10748aeb04b58e8f --token1 0xac51C4c48Dc3116487eD4BC16542e27B5694Da1b --intermediate 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619 --network polygon
 yarn hardhat add-intermediate-token --token0 0xc2132d05d31c914a87c6611c10748aeb04b58e8f --token1 0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270 --intermediate 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619 --network polygon
--- tasks/zap.ts
+++ tasks/zap.ts
@@ -1,4 +1,4 @@
-import { QUICKSWAP } from "./../test/utils/constants";
+import { QUICKSWAP, SUSHISWAP } from "./../test/utils/constants";
 import { task } from "hardhat/config";
 import { ZapMiniV2__factory } from "../typechain";
 
@@ -32,3 +32,31 @@ task("add-intermediate-token", "Add intermediate token")
     );
     console.log("\x1b[36m%s\x1b[0m", "tx", tx);
   });
+
+task("set-router-factory", "set protocol router and factory").setAction(
+  async function (_, hre) {
+    const { deployments, ethers } = hre;
+    const [deployer] = await ethers.getSigners();
+
+    const zapContract = await deployments.get("ZapMiniV2");
+
+    const zapInstance = await ZapMiniV2__factory.connect(
+      zapContract.address,
+      deployer,
+    );
+
+    const tx = await zapInstance.setFactoryAndRouter(
+      QUICKSWAP, // change me
+      "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
+      "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
+    );
+    console.log("\x1b[36m%s\x1b[0m", "tx", tx);
+
+    const tx2 = await zapInstance.setFactoryAndRouter(
+      SUSHISWAP, // change me
+      "0xc35dadb65012ec5796536bd9864ed8773abc74c4",
+      "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+    );
+    console.log("\x1b[36m%s\x1b[0m", "tx2", tx2);
+  },
+);
